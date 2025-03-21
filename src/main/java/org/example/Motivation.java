a4d9cf15324ee9d384136136eb586d5a63a050b8
qkjm1 <kqk9269@gmail.com>
2025-03-21 15:54:21 +0900

    삭제 만들ㅇ다 말았음

---
+++ src/main/java/org/example/Motivation.java
@@ -0,0 +1,33 @@
+package org.example;
+
+class Motivation { //조립 틀
+    private int id;
+    private String body;
+    private String source;
+
+    public Motivation(int id, String body, String source) {
+        this.id = id;
+        this.body = body;
+        this.source = source;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+    public String getSource() {
+        return source;
+    }
+    public String getBody() {
+        return body;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setSource(String source) {
+        this.source = source;
+    }
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
